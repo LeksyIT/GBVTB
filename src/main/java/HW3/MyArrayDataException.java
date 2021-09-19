d78617ca1fc0958feb45e4dde7c8af53f9772ac9
LeksymonIT <LeksymonIT@gmail.com>
2021-09-19 13:21:49 +0300

    3 HW

---
+++ src/main/java/HW3/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package HW3;
+
+public class MyArrayDataException extends Exception{
+    public MyArrayDataException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/HW3/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package HW3;
+
+public class MyArraySizeException extends Exception{
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
