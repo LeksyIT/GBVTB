763f8c6d24dd463ce9ebcc541bb670070cb50f1f
LeksymonIT <LeksymonIT@gmail.com>
2021-10-02 01:57:56 +0300

    9 HW(fix)

--- src/main/java/hw9/tableCreater/WorkWithTable.java
+++ src/main/java/hw9/tableCreater/WorkWithTable.java
@@ -18,6 +18,7 @@ public class WorkWithTable {
     private static Connection connection = DBConfig.getConnection();
     private static ArrayList<Field> fieldArray = new ArrayList<>();
     private static String tableName;
+
     static {
         classToString.put(int.class, "INTEGER");
         classToString.put(long.class, "BIGINT");
@@ -52,7 +53,8 @@ public class WorkWithTable {
             exception.printStackTrace();
         }
     }
-    private static void getAnnotations(Object obj){
+
+    private static void getAnnotations(Object obj) {
         Class objClass = obj.getClass();
 
         Table annotationTable = (Table) objClass.getAnnotation(Table.class);
@@ -62,71 +64,44 @@ public class WorkWithTable {
         Field[] array = Arrays.stream(fields)
                 .filter(field -> field.isAnnotationPresent(Column.class))
                 .toArray(Field[]::new);
-        for (Field field:array) {
+        for (Field field : array) {
             fieldArray.add(field);
         }
     }
+
     public static void appendToTable(Object obj) {
         getAnnotations(obj);
         try {
             Statement createStatement = connection.createStatement();
             StringBuilder sqlRequest = new StringBuilder();
-            sqlRequest.append("CREATE TABLE " + tableName + " (");
+            sqlRequest.append("INSERT INTO " + tableName + "(");
 
             for (Field field : fieldArray) {
-                sqlRequest.append(field.getName())
-                        .append(" ")
-                        .append(classToString.get(field.getType()))
-                        .append(",");
+                field.setAccessible(true);
+                sqlRequest.append(field.getName()).append(" ,");
             }
 
             if (sqlRequest.charAt(sqlRequest.length() - 1) == ',') {
                 sqlRequest.deleteCharAt(sqlRequest.length() - 1);
             }
 
+            sqlRequest.append(")\nVALUES (");
+
+            for (Field field : fieldArray) {
+                field.setAccessible(true);
+                sqlRequest.append(" '")
+                        .append(field.get(obj).toString())
+                        .append("',");
+            }
+
+            if (sqlRequest.charAt(sqlRequest.length() - 1) == ',') {
+                sqlRequest.deleteCharAt(sqlRequest.length() - 1);
+            }
             sqlRequest.append(");");
+            System.out.println(sqlRequest.toString());
             createStatement.executeUpdate(sqlRequest.toString());
-        } catch (SQLException exception) {
+        } catch (SQLException | IllegalAccessException exception) {
             exception.printStackTrace();
         }
-    }
-
-    private static String getStingFields1(Class<? extends TestTable> cS) {
-        StringBuilder str = new StringBuilder();
-        Field[] classFields = cS.getDeclaredFields();
-        for (Field field : classFields) {
-            str.append(field.getName())
-                    .append(", ");
-        }
-
-        if (str.charAt(str.length() - 2) == ',') {
-            str.deleteCharAt(str.length() - 2);
-            str.deleteCharAt(str.length() - 1);
-        }
-
-        return " (" + str + ")";
-    }
-
-    private static String getStingFields2(Object cS) {
-        StringBuilder str = new StringBuilder();
-        Field[] classFields = cS.getClass().getDeclaredFields();
-        for (Field field : classFields) {
-            try {
-                field.setAccessible(true);
-                Object kekw = field.get(cS);
-                str.append(" '")
-                        .append(field.get(cS))
-                        .append("'")
-                        .append(",");
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
-
-        if (str.charAt(str.length() - 1) == ',') {
-            str.deleteCharAt(str.length() - 1);
-        }
-
-        return " (" + str + ")";
     }
 }
